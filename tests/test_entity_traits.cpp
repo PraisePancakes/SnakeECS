8752970a66bf67af3bc3ebe1dcf950c5778924ed
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-03-13 19:17:55 -0400

    some more entity_traits testing

--- tests/test_entity_traits.cpp
+++ tests/test_entity_traits.cpp
@@ -5,5 +5,9 @@
 static void TEST_ENTITY_TRAITS()
 {
     snek::entity_traits<std::uint64_t>::value_type e0;
+    static_assert(std::is_same_v<decltype(e0), uint64_t>, "same!");
+    static_assert(!std::is_same_v<decltype(e0), uint32_t>, "not same!");
     snek::entity_traits<std::uint32_t>::value_type e1;
+    static_assert(!std::is_same_v<decltype(e1), uint64_t>, "same!");
+    static_assert(std::is_same_v<decltype(e1), uint32_t>, "not same!");
 };
